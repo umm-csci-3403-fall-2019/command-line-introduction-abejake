432fbcc3d901faacda7f19207fe06d8e9454d31e
abenezermonjor <monjo003@morris.umn.edu>
2019-09-03 16:30:05 -0500

    compiling the C program with gcc

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -2,5 +2,11 @@
 num=$1 
 dir=$2
 
-
 tar xfz NthPrime.tgz -C $dir
+
+cd $dir
+cd NthPrime
+
+gcc -Wall nth_prime.h main.c nth_prime.c -o NthPrime
+#wall is an abb. for write to all
+#-o echoes all the commands before executing
